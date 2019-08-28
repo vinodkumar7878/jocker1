f9f5e875b77c3dd95cdb191e769ebf2a307abec5
vinodkumar7878 <53477059+vinodkumar7878@users.noreply.github.com>
2019-08-28 17:47:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// this is for the first joker function
+
+function start6() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function end6() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+// this is for the second joker function
+
+
+
+function start() {
+  document.getElementById("over").style.display = "block";
+}
+
+function end() {
+  document.getElementById("over").style.display = "none";
+}
+
+// this is for the third joker function
+
+
+function start1() {
+  document.getElementById("overflow").style.display = "block";
+}
+
+function end1() {
+  document.getElementById("overflow").style.display = "none";
+}
+
+
+
+// this is for the forth joker function
+
+
+function start2() {
+  document.getElementById("overflow1").style.display = "block";
+}
+
+function end2() {
+  document.getElementById("overflow1").style.display = "none";
+}
+
+// this is for the fifth joker function
+
+
+function start3() {
+  document.getElementById("overflow2").style.display = "block";
+}
+
+function end3() {
+  document.getElementById("overflow2").style.display = "none";
+}
+
+
+// this is for the sixth joker function
+
+
+function start4() {
+  document.getElementById("overflow3").style.display = "block";
+}
+
+function end4() {
+  document.getElementById("overflow3").style.display = "none";
+}
+
+
+// this is for the last joker function
+
+
+function start5() {
+  document.getElementById("overflow4").style.display = "block";
+}
+
+function end5() {
+  document.getElementById("overflow4").style.display = "none";
+}
